1831a585bbff041bcc7be4fad11a65489049ddf2
MrChhoke <vladbondar16133@gmail.com>
2022-08-28 16:48:08 +0300

    a simple webapp

---
+++ src/main/java/com/epam/cash/register/controller/filter/CharacterEncodingFilter.java
@@ -0,0 +1,23 @@
+package com.epam.cash.register.controller.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CharacterEncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request,response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
