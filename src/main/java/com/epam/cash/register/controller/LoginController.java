22e13978b2977a54ce7e57ce61f51fe70805f42b
MrChhoke <vladbondar16133@gmail.com>
2022-09-01 01:00:10 +0300

    A Simple Authorization

---
+++ src/main/java/com/epam/cash/register/controller/LoginController.java
@@ -0,0 +1,36 @@
+package com.epam.cash.register.controller;
+
+import com.epam.cash.register.entity.User;
+import com.epam.cash.register.service.UserService;
+import com.epam.cash.register.service.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@WebServlet(urlPatterns = {"/login", "/log"})
+public class LoginController extends HttpServlet {
+
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String[]> map = req.getParameterMap();
+
+        if (userService.isUserExists(map.get("username")[0])){
+            User user = userService.findUserByName(map.get("username")[0]);
+            if(user.getPassword().equals(map.get("password")[0])){
+                req.getSession().setAttribute("user",user);
+            }
+        }
+    }
+}
---
+++ src/main/webapp/jsp-pages/login.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form method="post" action="/login">
+        <label>Login</label>
+        <input type="text" name="login"/>
+        <label>Password</label>
+        <input type="text" name="password"/>
+        <button>Login</button>
+    </form>
+</body>
+</html>
