b95162270af8188ac8382a8bb8ba4f747c954120
MrChhoke <vladbondar16133@gmail.com>
2022-09-01 20:38:48 +0300

    Created ProductService

---
+++ src/main/java/com/epam/cash/register/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.epam.cash.register.service;
+
+import com.epam.cash.register.entity.Product;
+import com.epam.cash.register.entity.User;
+import com.epam.cash.register.exception.ProductNotFoundException;
+import com.epam.cash.register.exception.UserNotFoundException;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+
+public interface ProductService {
+    List<Product> findAll() throws IOException;
+
+    Product findById(long id) throws IOException;
+
+    List<Product> findById(long... id) throws IOException;
+
+    List<Product> findById(List<Long> id) throws IOException;
+
+    Product findByCode(String code) throws IOException;
+
+    Product findByUkrainianTitle(String ukrainianTitle) throws IOException;
+
+    Product findByEnglishTitle(String englishTitle) throws IOException;
+
+    List<Product> findAllOutStock() throws IOException;
+
+    List<Product> findAllBetweenDate(Date from, Date to) throws IOException;
+
+    List<Product> findAllBetweenPrice(double from, double to) throws IOException;
+
+    List<Product> findAllAddedByUser(User user) throws IOException;
+
+    List<Product> findAllAddedByUser(String username) throws IOException;
+
+    void insert(Product product) throws IOException;
+
+    void update(Product product) throws IOException;
+
+    void delete(long product_id) throws IOException;
+
+    void delete(String code) throws IOException;
+}
---
+++ src/main/java/com/epam/cash/register/service/ProductServiceImpl.java
@@ -0,0 +1,187 @@
+package com.epam.cash.register.service;
+
+import com.epam.cash.register.dao.PostqreSQLProductDAO;
+import com.epam.cash.register.dao.ProductDAO;
+import com.epam.cash.register.entity.Product;
+import com.epam.cash.register.entity.User;
+import com.epam.cash.register.util.DBUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.naming.NamingException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+
+    private static final Logger log = LogManager.getLogger(ProductServiceImpl.class);
+
+    private ProductDAO productDAO;
+
+    public ProductServiceImpl() {
+        productDAO = new PostqreSQLProductDAO();
+    }
+
+    @Override
+    public List<Product> findAll() throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findAll(connection);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public Product findById(long id) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findById(connection,id);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public List<Product> findById(long... id) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findById(connection,id);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public List<Product> findById(List<Long> id) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findById(connection,id);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public Product findByCode(String code) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findByCode(connection,code);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public Product findByUkrainianTitle(String ukrainianTitle) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findByUkrainianTitle(connection,ukrainianTitle);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public Product findByEnglishTitle(String englishTitle) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findByEnglishTitle(connection,englishTitle);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public List<Product> findAllOutStock() throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findAllOutStock(connection);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public List<Product> findAllBetweenDate(Date from, Date to) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findAllBetweenDate(connection,from,to);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public List<Product> findAllBetweenPrice(double from, double to) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findAllBetweenPrice(connection,from,to);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public List<Product> findAllAddedByUser(User user) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findAllAddedByUser(connection,user);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public List<Product> findAllAddedByUser(String username) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            return productDAO.findAllAddedByUser(connection,username);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public void insert(Product product) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            productDAO.insert(connection,product);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public void update(Product product) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            productDAO.update(connection,product);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public void delete(long product_id) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            productDAO.delete(connection,product_id);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+
+    @Override
+    public void delete(String code) throws IOException {
+        try(Connection connection = DBUtil.getConnection()){
+            productDAO.delete(connection,code);
+        }catch (SQLException | NamingException exp){
+            log.error("Ooops, something is wrong", exp);
+            throw new IOException("Ooops, something is wrong", exp);
+        }
+    }
+}
