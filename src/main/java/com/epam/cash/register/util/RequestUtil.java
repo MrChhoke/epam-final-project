8f4fafb8220d39cef705d05fa03e32e67ee1b338
MrChhoke <vladbondar16133@gmail.com>
2022-09-28 22:04:11 +0300

    RequestUtil. This class helps to process request

---
+++ src/main/java/com/epam/cash/register/util/RequestUtil.java
@@ -0,0 +1,35 @@
+package com.epam.cash.register.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestUtil {
+
+    public static Map<String, String> getMapFromBody(InputStream inputStream) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            Map<String, String> map = new HashMap<>();
+            String lineInfo;
+            String lineKey = null;
+            while ((lineInfo = bufferedReader.readLine()) != null) {
+                if (lineInfo.contains("Content-Disposition: form-data;")) {
+                    lineKey = lineInfo
+                            .replace("Content-Disposition: form-data; name=\"", "")
+                            .replace("\"", "");
+                    continue;
+                }
+                if (lineInfo.length() == 0 || lineInfo.contains("WebKit")) {
+                    continue;
+                }
+                map.put(lineKey, lineInfo);
+            }
+            return map;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
